752d320da2b72b5ff0b55429118ad0321be23b9e
amitdutta <duttaamit356@gmail.com>
2024-05-18 23:44:25 +0530

    create listing fuctionality added

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -27,7 +27,22 @@ const CreateListing = () => {
     regularPrice,
     discountedPrice,
   } = formData;
-  function onChange() {}
+  function onChange(e) {
+    let boolean = null;
+    if (e.target.value === "true") {
+      boolean = true;
+    }
+    if (e.target.value === "false") {
+      boolean = false;
+    }
+
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }));
+    }
+  }
   return (
     <main className="max-w-md px-2 mx-auto">
       <h1 className="text-3xl text-center mt-6 font-bold">Create a Listing</h1>
